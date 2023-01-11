21158cfcdb5c61260e8cbfa8b90e82af8b207754
GobPython <gobreg@rambler.ru>
2023-01-11 15:11:17 +0300

    Домашнее задание к Уроку 5

--- les1.py
+++ les1.py
@@ -1,1 +1,21 @@
-ertertert
+# Домашнее задание после Урока 5
+user = "PythonStudent" # str
+print(user,type(user))
+dog = "Pit" # str
+print(dog,type(dog))
+done = 123 # int
+print(done,type(done))
+cat = 55 # int
+print(cat,type(cat))
+good = True
+print(good,type(good))
+notgood = False
+print(notgood,type(notgood))
+pusto = None
+print(pusto,type(pusto))
+pusto_ = None
+print(pusto_,type(pusto_))
+chast = 13.1
+print(chast,type(chast))
+chast_ = 1324.5
+print(chast_,type(chast_))
