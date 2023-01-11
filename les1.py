778ae09a103c2b298adddeb11f8e59282068238d
GobPython <gobreg@rambler.ru>
2023-01-11 16:37:27 +0300

    Домашнее задание к Уроку 6

--- les1.py
+++ les1.py
@@ -1,23 +1,6 @@
-# Домашнее задание после Урока 5
-user = "PythonStudent" # str
+# Домашнее задание после Урока 6
+user="123" # str
 print(user,type(user))
-dog = "Pit" # str
-print(dog,type(dog))
-done = 123 # int
-print(done,type(done))
-cat = 55 # int
-print(cat,type(cat))
-good = True
-print(good,type(good))
-notgood = False
-print(notgood,type(notgood))
-pusto = None
-print(pusto,type(pusto))
-pusto_ = None
-print(pusto_,type(pusto_))
-chast = 13.1
-print(chast,type(chast))
-chast_ = 1324.5
-print(chast_,type(chast_))
-
-# проверка связи
+print("Преобразуем str в int")
+chislo=int(user) # преобразуем str в int
+print(chislo,type(chislo))
