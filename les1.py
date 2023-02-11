da7fd726def2cb4c6be19771fcf3eeeb55ab69f4
Goblin <gobreg@rambler.ru>
2023-02-12 01:16:07 +0300

    Домашнее задание после Урока 34

--- les1.py
+++ les1.py
@@ -1,5 +1,7 @@
 # Домашнее задание после Урока 34
 # Добавить абстрактный класс Animal и переопределить в нём все будущие абстрактные методы.
+
+
 from abc import abstractmethod
 class Animal:
     def __init__(self):
