1a7c4b2b5713af69f0c76ef3dee44400d0df6ea1
Goblin <gobreg@rambler.ru>
2023-02-11 19:30:07 +0300

    Домашнее задание после Урока 28

--- les1.py
+++ les1.py
@@ -1,18 +1,21 @@
-# Домашнее задание после Урока 27 и Практики
-# Написать функцию, которая просит ввести имя и выводит на экран "Привет и введённое имя".
-# Далее написать к функции декоратор, который изменяет функцию и переводит имя в заглавные буквы.
+# Домашнее задание после Урока 28
+# Создать класс и вызвать пять объектов этого класса.
+class World:
+    def __init__(self, country, city, citizen):
+        self.country = country
+        self.city = city
+        self.citizen = citizen
+    def karta(self):
+        print('В городе ' + self.city + ' (' + self.country + ') проживает ' + str(self.citizen) + ' млн. жителей.')
 
-def NAME(func):
-    def upper():
-        func()
-        print('Привет', a.upper())
-    return upper
+count1 = World("Беларусь", "Минск", 3)
+count2 = World('Россия', 'Москва', 10)
+count3 = World('Польша', 'Варшава', 11)
+count4 = World('Германия', 'Белрлин', 15)
+count5 = World('Россия', 'Новосибирск', 1)
 
-@NAME
-def name():
-    global a
-    a = input('Введите имя: ')
-    print('Привет', a)
-    return
-
-name()
+count1.karta()
+count2.karta()
+count3.karta()
+count4.karta()
+count5.karta()
